6bb8aeaa515319db148158b85ce9147182272ec0
velskii <posixie@gmail.com>
2022-02-18 17:55:52 -0500

    make oceanbehaviour work

---
+++ Assets/[Scripts]/Boundary.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct Boundary 
+{
+    public float top;
+    public float right;
+    public float bottom;
+    public float left;
+}
--- Assets/[Scripts]/OceanBehaviour.cs
+++ Assets/[Scripts]/OceanBehaviour.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class OceanBehaviour : MonoBehaviour
 {
+    public float verticalSpeed;
+    public Boundary boundary;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+        Checkbounds();
+    }
+
+    public void Move()
+    {
+        Vector2 currentPosition = transform.position;
+        currentPosition += new Vector2(0.0f, verticalSpeed);
+        transform.position = currentPosition;
+    }
+
+    public void Checkbounds()
+    {
+        if (transform.position.y < boundary.bottom)
+        {
+            ResetObject();
+        }
+    }
+
+    public void ResetObject()
+    {
+        transform.position = new Vector2(0.0f, boundary.top);
     }
 }
